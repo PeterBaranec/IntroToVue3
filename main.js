3ef29531af424c397f3c9c4036ad8686217e9b65
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-06-08 15:34:34 +0200

    computed properties for title, image and inStock

--- main.js
+++ main.js
@@ -2,12 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      image: "./assets/images/socks_blue.jpg",
-      inStock: false,
+      brand: "Vue Mastery",
+      selectedVariant: 0,
+      onSale: false,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
       cart: 0,
     };
@@ -21,8 +32,22 @@ const app = Vue.createApp({
         this.cart -= 1;
       }
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    sale() {
+      return this.brand + " " + this.product + " " + "is on sale!";
     },
   },
 });
